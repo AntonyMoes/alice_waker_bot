dbaacfb0da70404073c4b99b5bfd68f269fb6665
AntonyMo <moantonymo@gmail.com>
2018-07-07 00:26:04 +0300

    listening socket created

--- bot.py
+++ bot.py
@@ -35,8 +35,11 @@ class BotHandler:
 greet_bot = BotHandler("594760722:AAE_epRLd_DYiag967BWF6bu9zeBebspQxw")
 greetings = ('hello')
 now = datetime.datetime.now()
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("0.0.0.0", int(os.environ.get("$PORT", 5000))))
+s = socket.socket()
+s.bind(('', int(os.environ.get("$PORT", 5000))))
+print "test1"
+s.listen(1);
+print "test2"
 
 def main():
     new_offset = None
